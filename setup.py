9c2dd40e61e0581c92ced24c1592b2656de8baaf
naveenkrishnan840 <naveenkrishnan840@gmail.com>
2021-10-02 11:31:07 +0530

    utiles file added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='src',
+	version="0.0.1",
+	description="DVC",
+	long_description="DVC_DEMO",
+	author="naveen",
+	author_email="naveenkrishnan840@gmail.com",
+	url="https://github.com/naveenkrishnan840/DVC-ML-DEMO-AIOPS",
+	packages='src',
+	python_requires = ">=3.7",
+	install_requires=[
+		"dvc", "pandas", "scikit-learn"
+	],
+)
---
+++ src/utils/all_utils.py
@@ -0,0 +1,11 @@
+import yaml
+import os
+
+
+def read_display(conf_path: str) -> dict:
+	with open(conf_path, 'r') as yaml_file:
+		content = yaml.safe_load(yaml_file)
+
+	return content
+	
+		
